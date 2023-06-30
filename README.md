4cdefee8c8191ea5faac1dd0fc7a738263fbcae9
A Bevers <alisabevers@gmail.com>
2023-06-30 11:11:40 -0400

    created dog years converter and updated the readme

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# dog-years-converter
+# Dog Years Converter
+
+You can use this to calculate your age in dog years! Simply change the myAge variable to accurately represent your own age. In the terminal, run ```node app.js``` to see your age in dog years.
---
+++ app.js
@@ -0,0 +1,20 @@
+// This is my current age in human years
+let myAge = 25;
+
+// These are the early years of life, subject to change
+let earlyYears = 2;
+
+earlyYears = earlyYears * 10.5;
+
+// My current age minus the early years equal laterYears, subject to change
+let laterYears = myAge - 2;
+
+// To calculate my age in dog years, I multiplied my later years times 4.
+laterYears = laterYears * 4
+
+// by adding my earlyYears and laterYears together, we get my age in dog years:
+myAgeInDogYears = earlyYears + laterYears;
+
+const myName = 'Ali'.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
